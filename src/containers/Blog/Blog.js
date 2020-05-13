7244a02de724c4b72829e3da96b9bfa59cb7bba5
Martin Papy <martin.papy@gmail.com>
2020-05-13 11:36:38 +0700

    Using Axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,15 +4,18 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-import axios from 'axios';
+//import axios from 'axios';
+import axios from '../../axios';
 
 class Blog extends Component {
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null,
+        error: false
     };
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
+        axios.get('/posts')
             .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
@@ -21,13 +24,28 @@ class Blog extends Component {
                     }
                 });
                 this.setState({posts:updatedPosts});
+            })
+            .catch(error => {
+                //console.log(error);
+                this.setState({error:true})
             });
     }
 
+    postSelectedHandler(id) {
+        this.setState({selectedPostId:id})
+    }
+
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author}/>;
-        });
+        let posts = <p style={{textAlign:'center', colo:'red'}}>Something went wrong...</p>
+        if(!this.state.error) {
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>;
+            });
+        }
 
         return (
             <div>
@@ -35,7 +53,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization']='AUTH TOKEN';
+axios.defaults.headers.post['Content-Type']='application/json';
+
+axios.interceptors.request.use( request => {
+    console.log('[Interceptor onRequest]');
+    console.log(request);
+    return request;
+}, error => {
+    console.log('[Interceptor onError / Request going out]');
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use( response => {
+    console.log('[Interceptor onResponse]');
+    console.log(response);
+    return response;
+}, error => {
+    console.log('[Interceptor onError / Response going in]');
+    console.log(error);
+    return Promise.reject(error);
+});
+
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
