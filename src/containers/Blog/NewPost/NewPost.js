12c18c790cf8a297f6b34900b88ffc854363d331
Martin Papy <martin.papy@gmail.com>
2020-05-14 15:41:07 +0700

    Adding redirections

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import {Redirect} from "react-router-dom";
+
 import './NewPost.css';
 
 class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'Max',
+        submitted: false
     }
 
     postDataHandler = () => {
@@ -19,7 +22,11 @@ class NewPost extends Component {
         axios.post('/posts', data)
             .then(
                 response => {
-                    console.log(response);
+                    console.log('[Post Submitted Successfully]', response);
+                    // this.setState({submitted:true})
+                    //this.props.history.push('/');
+                    this.props.history.replace('/');
+
                 }
             );
     }
@@ -29,8 +36,15 @@ class NewPost extends Component {
     }
 
     render () {
+
+        // let redirect = null;
+        // if(this.state.submitted){
+        //     redirect = <Redirect to='/' />
+        // }
+
         return (
             <div className="NewPost">
+                {/*{redirect}*/}
                 <h1>Add a Post</h1>
                 <label>Title</label>
                 <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
